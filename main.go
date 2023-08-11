8ba89b6e4e7f21465290f033321523138167820f
abtinokhovat <abtinokhovat@gmail.com>
2023-08-11 21:28:42 +0330

    implement cli interface

--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"branches-cli/cmd"
+	"branches-cli/internal/branch"
+	"branches-cli/internal/region"
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	command := flag.String("command", "exit", "Command name that can be executed")
-	region := flag.String("region", "Tehran", "the city that you want to retrieve the data of branches from")
+	fmt.Println(`
+	
+██████╗██████╗ █████╗███╗   ██╗████████╗  ██╗     ████████╗    ██╗
+██╔══████╔══████╔══██████╗  ████╔════██║  ██║    ██╔════██║    ██║
+██████╔██████╔█████████╔██╗ ████║    ██████████████║    ██║    ██║
+██╔══████╔══████╔══████║╚██╗████║    ██╔══██╚════██║    ██║    ██║
+██████╔██║  ████║  ████║ ╚████╚████████║  ██║    ╚███████████████║
+╚═════╝╚═╝  ╚═╚═╝  ╚═╚═╝  ╚═══╝╚═════╚═╝  ╚═╝     ╚═════╚══════╚═╝
+	`)
 
+	fmt.Println("Written by ABTIN OKHOVAT")
+
+	com := flag.String("command", "status", "Command name that can be executed")
+	reg := flag.String("reg", "Tehran", "the city that you want to retrieve the data of branches from")
 	flag.Parse()
-	fmt.Println("command:", *command)
-	fmt.Println("region:", *region)
+
+	// setup
+	scanner := bufio.NewScanner(os.Stdin)
+	branchService := branch.BuildService()
+	regionService := region.BuildService()
+
+	command := cmd.NewCommand(regionService, branchService, scanner)
+
+	for {
+		command.Execute(*com, *reg)
+
+		fmt.Println("\n\nenter another command: ")
+		scanner.Scan()
+		if scanner.Text() != "change" {
+			*com = scanner.Text()
+		} else {
+			*reg = scanner.Text()
+		}
+	}
 }
