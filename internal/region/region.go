111b0e520c228a634c2c82e89d299dead0e716ae
abtinokhovat <abtinokhovat@gmail.com>
2023-08-11 21:27:53 +0330

    add needed data to region class and string method

--- internal/region/region.go
+++ internal/region/region.go
@@ -1,6 +1,14 @@
 package region
 
+import "fmt"
+
+const StoragePath = "storage/region.json"
+
 type Region struct {
-	Id   int
-	Name string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func (r *Region) String() string {
+	return fmt.Sprintf("#%d: %s", r.Id, r.Name)
 }
